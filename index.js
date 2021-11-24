7d5fa4bd7a1491b976198006934e88a09386219f
Daniel Schmidt <d.schmidt20@yahoo.com>
2021-11-23 20:38:57 -0800

    commit tree c14cf45e7615fd455233d2be7358c6c005a07f21

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+ const newArray = drivers.slice(0,2)
+   return newArray
+}
+const returnLastTwoDrivers = function (drivers) {
+    const newArray = drivers.slice(2)
+      return newArray
+   }
+   const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+    selectingDrivers
+
+    function createFareMultiplier(num) {
+        return function(fare) {
+            return fare * num;
+        }
+    }
+const fareDoubler= createFareMultiplier(2)
+ fareDoubler   
+ const fareTripler= createFareMultiplier(3)
+ fareTripler  
+
+
+ function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
